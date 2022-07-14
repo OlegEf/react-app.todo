0b96b57afecaf24b7657b7566f580103f096f6e0
Oleg <5tg6yy@gmail.com>
2022-07-15 00:47:01 +0300

    added component Todolist inside el element

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-	<div>
-		<h1>My Todo list</h1>
-		<input placeholder = "search" />
+const Todolist  = () => {
+	return (
 		<ul>
 			<li>Learn React</li>
 			<li>Build Awesome App</li>
 		</ul>
+	);
+};
+
+const el = (
+	<div>
+		<h1>My Todo list</h1>
+		<input placeholder = "search" />
+		<Todolist/>
 	</div>
 );
 
