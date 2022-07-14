3b9d7e6198bc86447f65e30f5679e3c204109a73
Oleg <5tg6yy@gmail.com>
2022-07-14 18:23:06 +0300

    t commit --amend 'added import react, reactDOM; jsx element: el Hello World ; reactDOM.render'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello React');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = <h1>Hello World</h1>;
+
+ReactDOM.render(el,
+  document.getElementById('root'));
