18a5cae284dfb56722bc39ad3b825f87ff7842f7
Oleg <5tg6yy@gmail.com>
2022-07-16 22:16:34 +0300

    1) Added a components folder to src\, 2) In which I created the files: app-header, search-panel, todo-list, todo-list-item, 3) Placed components in each file and added react import and export to each file, 4) added import of all components to index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const AppHeader  = () => {
-	return (<h1>My Todo list</h1>);
-};
-
-const SearchPanel  = () => {
-	const searchText = 'Type here to search';
-	const searchStyle = { fontSize: '20px' };
-	
-	return <input 
-		style = {searchStyle}
-		placeholder = {searchText} />;
-};
-
-const Todolist  = () => {
-	
-	const items = [ 'Drink Coffee', 'Build Awesome App' ];
-	
-	return (
-		<ul>
-			<li>{ items[0] }</li>
-			<li>{ items[1] }</li>
-		</ul>
-	);
-};
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
 const App = () => {
-	return (
-	<div>
-		<AppHeader/>
-		<SearchPanel/>
-		<Todolist/>
-	</div>
-	);
+
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
 };
-	
-ReactDOM.render(<App/>,
+
+ReactDOM.render(<App />,
   document.getElementById('root'));
