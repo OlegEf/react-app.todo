9a0f8457f326e2c8930722d45122a3502a70d4e8
Oleg <5tg6yy@gmail.com>
2022-07-16 23:37:31 +0300

    1) added the label property, object props, 2) passed the label property to the todo-list-item

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import TodoListItem from './todo-list-item';
 
-const TodoList = () => {
+const TodoList = (props) => {
   return (
     <ul>
-      <li><TodoListItem /></li>
-      <li><TodoListItem /></li>
+      <li><TodoListItem label = "Drink Coffee" /></li>
+      <li><TodoListItem label = "Build React App" /></li>
     </ul>
   );
 };
